91a9f48a082e87c6ed80289dae9ba6d11a0aa9a8
eatech2728-sys <eatech2728@gmail.com>
2026-09-13 23:54:29 +0800

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,7 +1,7 @@
 // ============================================================
 // GANTI URL DI BAWAH INI dengan Web App URL Apps Script Anda
 // ============================================================
-const API_BASE = 'https://script.google.com/macros/s/AKfycbxG8xOBUOijRMUoCtDyn2-hEBL5damM2uGK6BD31PJWFMcsfdpKT_8be6s5KdsCRmvN/exec';
+const API_BASE = 'https://script.google.com/macros/s/AKfycbyTc2ZebcADtl7aR48puMf-xLnQ24VgJahSpoDbwn3dpbF1xICFBOK16rZPTkcUyQHG/exec';
 
 let token = localStorage.getItem('sh_token') || null;
 let user = JSON.parse(localStorage.getItem('sh_user') || 'null');
@@ -158,6 +158,7 @@ function goTo(tab) {
   if (tab === 'riwayat') muatRiwayat();
   if (tab === 'izin') muatIzin();
   if (tab === 'profil') muatProfil();
+  if (tab === 'chat' && typeof masukChat === 'function') masukChat();
   if (tab === 'presensi') siapkanKameraCard(); else hentikanKamera();
 }
 
